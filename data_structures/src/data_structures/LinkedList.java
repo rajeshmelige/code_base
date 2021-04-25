9bb8555cf5dc0214112d3a7dc4b2a4d0e61660b5
rajeshmelige <rajeshbmelige1@gmail.com>
2021-04-25 20:46:59 +0530

    initial commit for data_structures

---
+++ data_structures/src/data_structures/LinkedList.java
@@ -0,0 +1,64 @@
+package data_structures;
+
+public class LinkedList {
+	Node head;
+	public static class Node {
+		int data;
+		Node next;
+		
+		Node(int data) {
+			this.data = data;
+		}
+	}
+	
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		insert(list,0);
+		insert(list,1);
+		insert(list,2);
+		insert(list,3);
+		insert(list,4);
+		insert(list,5);
+		insert(list,6);
+		insert(list,7);
+		insert(list,8);
+		
+		int value = get(list,4);
+		System.out.println(value);
+		
+		//printList(list);
+	}
+
+	private static int get(LinkedList list, int i) {
+		Node current = list.head;
+		int n = 0;
+		
+		while(n < i) {
+			current = current.next;
+			n++;
+		}
+		return current.data;
+	}
+
+	private static void printList(LinkedList list) {
+		Node last = list.head;
+		while(last != null) {
+			System.out.println(last.data);
+			last = last.next;
+		}
+	}
+
+	private static void insert(LinkedList list, int data) {
+		Node newNode = new Node(data);
+		newNode.next = null;
+		if(list.head == null) {
+			list.head = newNode;
+		} else {
+			Node last = list.head;
+			while(last.next != null) {
+				last = last.next;
+			}
+			last.next = newNode;
+		}
+	}
+}
