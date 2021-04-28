d5012039083b6e6ed8473a07caf5fb8c3222eb33
rajeshmelige <rajeshbmelige1@gmail.com>
2021-04-28 19:49:52 +0530

    Quotient problem statements initial commit

---
+++ problem_statements/src/problem_statements/Quotient1.java
@@ -0,0 +1,19 @@
+package problem_statements;
+
+public class Quotient1 {
+	
+	/*
+	 required number of persons to fit = x
+	 Sizes of Umbrella = [2,3,4]
+	 
+	 if x=3, and sizes = [2,3,4] print 1
+	 	x=5 and sizes = [2,3,4] print 2
+	 	x=4 and sizes = [2,3,4] print -1	
+	*/
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ problem_statements/src/problem_statements/Quotient2.java
@@ -0,0 +1,19 @@
+package problem_statements;
+
+public class Quotient2 {
+	
+	/*
+	 * 	Segment length input x
+	 * 	intput array 
+	 * 
+	 * arr = [4,5,6,2,8,9]
+	 * x = 3
+	 * 
+	 * segments = [4,5,6],[5,6,2],[6,2,8],[2,8,9]
+	 * minimum of each segment = [4,2,2,2]
+	 * maximun of minimum values = 4
+	 * 
+	 * return 4;
+	 */
+
+}
